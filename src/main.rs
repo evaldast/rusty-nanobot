6ac0e008efcdea4e390dada5deab68272a67f578
Evaldas <evaldas.tyla@gmail.com>
2018-04-06 13:29:55 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,110 @@
+#![feature(plugin, decl_macro)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::{Json, Value};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Event {
+    #[serde(rename = "configCompleteRedirectUrl", default)]
+    config_complete_redirect_url: String,
+
+    #[serde(rename = "eventTime")]
+    event_time: String,
+
+    #[serde(default)]
+    message: Message,    
+
+    space: Space,
+    token: String,
+
+    #[serde(rename = "type")]
+    event_type: String,
+
+    user: Sender
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Space {
+    name: String,
+
+    #[serde(rename = "type")]
+    message_type: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Sender {
+    #[serde(rename = "avatarUrl")]
+    avatar_url: String,
+
+    #[serde(rename = "displayName")]
+    display_name: String,
+
+    email: String,
+    name: String,
+    
+    #[serde(rename = "type")]
+    sender_type: String       
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Thread {
+    name: String,
+
+    #[serde(rename = "retentionSettings")]
+    retention_settings: RetentionSettings
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RetentionSettings {
+    state: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Message {
+    #[serde(rename = "createTime")]
+    create_time: String,
+
+    name: String,
+    sender: Sender,
+    space: Space,
+    text: String,
+    thread: Thread
+}
+
+//impl Default for Message {
+//    fn default() -> Message { Message::new() }
+//}
+
+#[derive(Serialize, Deserialize)]
+struct ResponseMessage {
+    text: String
+}
+
+#[post("/hello", format = "application/json", data = "<event>")]
+fn post_json(event: Json<Event>) -> Json<ResponseMessage> {
+    match event.0.event_type.trim() {
+       "ADDED_TO_SPACE" => return  Json(ResponseMessage { text: "Thanks for adding me, ".to_string() + &event.0.user.display_name }),
+       "MESSAGE" => return  Json(ResponseMessage { text: "Thanks for messaging me, ".to_string() + &event.0.user.display_name }),
+       _ => Json(ResponseMessage { text: "Sorry, did not understand that".to_string() }) 
+    };
+
+    println!("{:?}", event.0);
+    Json(ResponseMessage { text: "Būk pasveikintas, ".to_string() + &event.0.user.display_name })
+}
+
+#[get("/")]
+fn moo() -> &'static str {
+    "Mooo, from Uboontoo!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![post_json, moo]).launch();
+}
+
+fn default_resource() -> String {
+    String::new()
+}
