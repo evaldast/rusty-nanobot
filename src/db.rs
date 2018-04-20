ae406482161917dd60edd74e8055cd3a9b392115
unknown <evaldas.tyla@gmail.com>
2018-04-20 21:44:52 +0300

    Database initializer added

--- src/db.rs
+++ src/db.rs
@@ -2,18 +2,18 @@ use rusqlite::{Connection, Error};
 use std::sync::Mutex;
 use node::NewAccount;
 
-// fn init_database(conn: &Connection) {
-//     conn.execute(
-//         "CREATE TABLE accounts (
-//                   id                INTEGER PRIMARY KEY,
-//                   account           TEXT NOT NULL,
-//                   public            TEXT NOT NULL,
-//                   private           TEXT NOT NULL,
-//                   email             TEXT UNIQUE NOT NULL
-//                   )",
-//         &[],
-//     ).expect("create accounts table");
-// }
+fn init_database(conn: &Connection) {
+    conn.execute(
+        "CREATE TABLE accounts (
+                  id                INTEGER PRIMARY KEY,
+                  account           TEXT NOT NULL,
+                  public            TEXT NOT NULL,
+                  private           TEXT NOT NULL,
+                  email             TEXT UNIQUE NOT NULL
+                  )",
+        &[],
+    ).expect("create accounts table");
+}
 
 pub fn add_account(db_conn: &Mutex<Connection>, acc: NewAccount) -> Result<(), Error> {
     db_conn.lock()
@@ -26,6 +26,7 @@ pub fn add_account(db_conn: &Mutex<Connection>, acc: NewAccount) -> Result<(), E
 
 pub fn get_connection() -> Mutex<Connection> {
     let conn = Connection::open("sqlite\\main.database").unwrap();
+    init_database(&conn);
 
     return Mutex::new(conn);
 }
