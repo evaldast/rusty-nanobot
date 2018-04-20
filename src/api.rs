dd7cbfdd09136826fc919be8f77e450b48e4826b
unknown <evaldas.tyla@gmail.com>
2018-04-20 21:41:19 +0300

    Removed printing of every direct message

--- src/api.rs
+++ src/api.rs
@@ -81,7 +81,7 @@ struct ResponseMessage {
 
 #[post("/hello", format = "application/json", data = "<event>")]
 fn post_json(db_conn: State<Mutex<Connection>>, event: Json<Event>) -> Json<ResponseMessage> {
-    println!("{:?}", &event.0);
+    // println!("{:?}", &event.0);
 
     match event.0.event_type.trim() {
         "ADDED_TO_SPACE" => {
