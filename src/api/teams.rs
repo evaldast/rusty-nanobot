df30f3371df5f9f0c314840afcc2083557a305ec
Evaldas Tyla <Evaldas.Tyla@macaw.nl>
2018-06-05 12:38:32 +0300

    Updated test adaptive card

--- src/api/teams.rs
+++ src/api/teams.rs
@@ -147,7 +147,10 @@ struct TextBlock {
     weight: Option<String>,
     color: Option<String>,
     size: Option<String>,
-    spacing: Option<String>
+    spacing: Option<String>,
+
+    #[serde(rename = "horizontalAlignment")]
+    horizontal_alignment: Option<String>
 }
 
 #[derive(Serialize)]
@@ -181,6 +184,7 @@ struct ImageBlock {
 trait CardBody: erased_serde::Serialize {}
 impl CardBody for TextBlock {}
 impl CardBody for ColumnSet {}
+impl CardBody for ImageBlock {}
 
 serialize_trait_object!(CardBody);
 
@@ -299,7 +303,21 @@ fn get_test_adaptive_card() -> AttachmentAdaptive {
             card_type: "AdaptiveCard".to_string(),
             version: "1.0".to_string(),
             body: vec!(
-                 Box::new( TextBlock { body_type: "TextBlock".to_string(), text: "{action} {crypto}".to_string(), weight: Some("bolder".to_string()), color: None, size: None, spacing: None })
+                 Box::new(TextBlock { body_type: "TextBlock".to_string(), text: "{action} {crypto}".to_string(), weight: Some("bolder".to_string()), color: None, size: None, spacing: None, horizontal_alignment: None }),
+                 Box::new(ColumnSet { body_type: "ColumnSet".to_string(), separator: true, columns: vec!(
+                     Column { body_type: "Column".to_string(), width: "1".to_string(), items: vec!(
+                         Box::new(TextBlock { body_type: "TextBlock".to_string(), text: "Sender".to_string(), weight: None, color: None, size: None, spacing: None, horizontal_alignment: None }),
+                         Box::new(TextBlock { body_type: "TextBlock".to_string(), text: "{sender_id}".to_string(), weight: None, color: Some("accent".to_string()), size: Some("large".to_string()), spacing: Some("none".to_string()), horizontal_alignment: None })
+                     )},
+                     Column { body_type: "Column".to_string(), width: "1".to_string(), items: vec!(
+                         Box::new(TextBlock { body_type: "TextBlock".to_string(), text: "&nbsp".to_string(), weight: None, color: None, size: None, spacing: None, horizontal_alignment: None }),
+                         Box::new(ImageBlock { body_type: "Image".to_string(), url: "https://i.imgur.com/6J8tqcM.png".to_string(), size: "small".to_string(), spacing: "none".to_string() })
+                     )},
+                     Column { body_type: "Column".to_string(), width: "1".to_string(), items: vec!(
+                         Box::new(TextBlock { body_type: "TextBlock".to_string(), text: "Receiver".to_string(), weight: None, color: None, size: None, spacing: None, horizontal_alignment: Some("right".to_string()) }),
+                         Box::new(TextBlock { body_type: "TextBlock".to_string(), text: "{receiver_id}".to_string(), weight: None, color: Some("accent".to_string()), size: Some("large".to_string()), spacing: Some("none".to_string()), horizontal_alignment: Some("right".to_string()) })
+                     )}                    
+                 ) })
             )
         }
     }
